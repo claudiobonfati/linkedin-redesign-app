1782828d89470b4554eb93ec586ac67d94e5e820
Claudio Bonfati <claudio_bonfati@outlook.com>
2020-12-07 08:39:26 -0300

    Initial Commit

---
+++ pages/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Linkedin Redesign</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Linkedin Redesign
+        </h1>
+      </main>
+    </div>
+  );
+}
