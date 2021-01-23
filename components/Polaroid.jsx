95ac878114eecc7f01f74a2fd38c63196a6e89fc
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-01-23 11:46:01 -0300

    Refactor Polaroid to function component

--- components/Polaroid.jsx
+++ components/Polaroid.jsx
@@ -3,46 +3,44 @@ import PropTypes from 'prop-types';
 import styles from './Polaroid.module.sass';
 import ConditionalWrapper from '../utils/ConditionalWrapper';
 
-function Polaroid(props) {
-  return (
-    <div className={styles.wrapper}>
-      <ConditionalWrapper
-        condition={props.link}
-        wrapper={(children) => <a href={props.link} className={styles.linkWrapper}>{children}</a>}
-      >
-        {props.image
-        && (
-          <div className={styles.imageWrapper} style={{ padding: props.imagePadding }}>
-            <div className={styles.imageBox}>
-              <img
-                src={props.image}
-                alt="Cover"
-                className={styles.image}
-              />
-            </div>
+const polaroid = (props) => (
+  <div className={styles.wrapper}>
+    <ConditionalWrapper
+      condition={props.link}
+      wrapper={(children) => <a href={props.link} className={styles.linkWrapper}>{children}</a>}
+    >
+      {props.image
+      && (
+        <div className={styles.imageWrapper} style={{ padding: props.imagePadding }}>
+          <div className={styles.imageBox}>
+            <img
+              src={props.image}
+              alt="Cover"
+              className={styles.image}
+            />
           </div>
-        )}
-        {props.title
-        && (
-          <div className={`py-3 px-2 text-center ${styles.title}`}>
-            {props.title}
-          </div>
-        )}
-      </ConditionalWrapper>
-    </div>
-  );
-}
+        </div>
+      )}
+      {props.title
+      && (
+        <div className={`py-3 px-2 text-center ${styles.title}`}>
+          {props.title}
+        </div>
+      )}
+    </ConditionalWrapper>
+  </div>
+);
 
-Polaroid.propTypes = {
+polaroid.propTypes = {
   title: PropTypes.string,
   imagePadding: PropTypes.number,
   link: PropTypes.string,
 };
 
-Polaroid.defaultProps = {
+polaroid.defaultProps = {
   title: null,
   imagePadding: 0,
   link: null,
 };
 
-export default Polaroid;
+export default polaroid;
