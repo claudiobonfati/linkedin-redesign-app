08c34eed1b576e9c56460a38b51d7d66e6ef4e8f
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-02-06 11:52:39 -0300

    Create Chat's master component

---
+++ components/Chat/Chat.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Chat.module.sass';
+import BarContacts from './BarContacts';
+import BarDescription from './BarDescription';
+import BarChat from './BarChat';
+import { useChat } from '../../context/Chat';
+
+const myChat = (props) => {
+  const context = useChat();
+
+  return (
+    <div
+      className={`
+        ${styles.wrapper} 
+        ${context.data.fullScreen ? styles.fullScreen : ''} 
+        ${!context.data.contact ? styles.noUser : ''} 
+        ${styles[`Page${context.data.page}`]}
+      `}
+    >
+      <div className={styles.sideBarLeft}>
+        <BarContacts contacts={props.contacts} />
+      </div>
+      <div className={styles.center}>
+        {context.data.contact
+        && (
+          <BarChat />
+        )}
+      </div>
+      <div className={styles.sideBarRight}>
+        {context.data.contact
+        && (
+          <BarDescription />
+        )}
+      </div>
+    </div>
+  );
+};
+
+myChat.propTypes = {
+  contacts: PropTypes.array.isRequired,
+};
+
+export default myChat;
