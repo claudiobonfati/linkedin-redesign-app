033b351fb4eb64c53326ee11f842f292f89f5be1
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-02-14 15:14:28 -0300

    Added shortcut to 'Connect with me' at pages within 'me/' route

--- components/CurrentProfileOverview.jsx
+++ components/CurrentProfileOverview.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import ProfileOverview from './ProfileOverview';
 import { getSimpleUser } from '../graphql/hooks';
+import SimpleButton from './SimpleButton';
 
 class CurrentProfileOverview extends React.Component {
   constructor(props) {
@@ -35,17 +36,29 @@ class CurrentProfileOverview extends React.Component {
       <>
         {(this.state.user && !this.state.user.error && !this.state.user.loading)
         && (
-          <ProfileOverview
-            photo={this.state.user.data.photo}
-            name={this.state.user.data.name}
-            position={this.state.user.data.headline}
-            connections={this.state.user.data.connections}
-            views={653}
-            actionMyProfile={this.props.simple}
-            email={!this.props.simple ? this.state.user.data.email : null}
-            twitter={!this.props.simple ? this.state.user.data.twitter : null}
-            skype={!this.props.simple ? this.state.user.data.skype : null}
-          />
+          <>
+            <ProfileOverview
+              photo={this.state.user.data.photo}
+              name={this.state.user.data.name}
+              position={this.state.user.data.headline}
+              connections={this.state.user.data.connections}
+              views={653}
+              actionMyProfile={this.props.simple}
+              email={!this.props.simple ? this.state.user.data.email : null}
+              twitter={!this.props.simple ? this.state.user.data.twitter : null}
+              skype={!this.props.simple ? this.state.user.data.skype : null}
+            />
+            {!this.props.simple
+            && (
+              <div className="mt-4">
+                <SimpleButton
+                  text="Connect with me"
+                  to="https://www.linkedin.com/in/claudiobonfati/"
+                  outside
+                />
+              </div>
+            )}
+          </>
         )}
       </>
     );
