1e09a69218525407ea9cb1fd26afd20f5c0196ac
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-02-20 16:32:02 -0300

    Create new page 'messages/unread'

---
+++ pages/messages/unread.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Chat from '../../components/Chat/Chat';
+import { useChatUnreadUsersList } from '../../graphql/hooks';
+import { ChatProvider } from '../../context/Chat';
+
+function messagesUnread() {
+  const contacts = useChatUnreadUsersList(1);
+  let contactsList = null;
+
+  if (!contacts.loading
+      && !contacts.error
+      && contacts.data) {
+    contactsList = contacts.data;
+  }
+
+  return (
+    <motion.div
+      className="w-100 full-screen"
+      initial={{ opacity: 0.3 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0.3 }}
+    >
+      <div className="container full-screen">
+        <main className="row h-100">
+          <div className="col-12 py-0 py-sm-4 px-0 py-sm-3 h-100 position-static">
+            {contactsList
+            && (
+              <ChatProvider>
+                <Chat contacts={contactsList} />
+              </ChatProvider>
+            )}
+          </div>
+        </main>
+      </div>
+    </motion.div>
+  );
+}
+
+export default messagesUnread;
