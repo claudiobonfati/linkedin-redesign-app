deb16d0791b484ac0b200181fe165245c6bffcc2
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-03-06 19:10:29 -0300

    Create new component Loading

---
+++ components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Loading.module.sass';
+
+const loading = () => (
+  <div className={styles.wrapper}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default loading;
