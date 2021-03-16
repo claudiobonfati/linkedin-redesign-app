a4d186120649fbb1d6f43d5518e94e0efbfe48f9
Claudio Bonfati <claudio_bonfati@outlook.com>
2021-03-16 19:51:56 -0300

    Insert relative path to NothingFound background image

--- components/NothingFound.jsx
+++ components/NothingFound.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './NothingFound.module.sass';
 
 const nothingFound = (props) => (
-  <div className={styles.wrapper}>
+  <div className={styles.wrapper} style={{backgroundImage: `url(${process.env.BASE_PATH}/images/no-result-found.svg)`}} >
     <p className="text-center">
       {props.title}
       {props.subtitle && (
